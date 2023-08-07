25026d8410650d8684eae80a4d87fa3bdbea3f3b
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-08-07 17:47:08 -0400

    Completed ChallengeSet05

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace ChallengesWithTestsMark8
 {
@@ -12,21 +14,21 @@ namespace ChallengesWithTestsMark8
             {
                 startNumber = 1;
             }
-            
-            for (int i = startNumber + 1; ;i++)
+
+            for (int i = startNumber + 1; ; i++)
             {
                 if (i % n == 0)
                 {
                     return i;
                 }
-               
-                
+
+
             }
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-           
+
 
             foreach (var business in businesses)
             {
@@ -47,28 +49,93 @@ namespace ChallengesWithTestsMark8
             var ascendingOrder = numbers.OrderBy(number => number).ToArray();
             bool trueAscendingOrder = numbers.SequenceEqual(ascendingOrder);
 
-            return trueAscendingOrder; 
-            
+            return trueAscendingOrder;
+
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    if (i + 1 < numbers.Length)
+                    {
+                        sum += numbers[i + 1];
+                    }
+
+                }
+
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+
+            var justWords = words.Where(word => !string.IsNullOrWhiteSpace(word)).Select(word => word.Trim());
+            string sentence = string.Join(" ", justWords).Trim();
+
+            if (!string.IsNullOrEmpty(sentence) && !char.IsPunctuation(sentence[sentence.Length - 1]))
+            {
+                sentence += ".";
+            }
+
+            return sentence;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+
+            if (elements == null || elements.Count == 0)
+            {
+                return new double[0];
+            }
+            
+            List<double> every4th = new List<double>();
+
+            foreach (var item in elements)
+            {
+                if (item % 4 == 0)
+                {
+                    every4th.Add(item);
+                }
+            }
+            return every4th.ToArray();
         }
+
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
-        }
+            if (nums == null || nums.Length == 0 || nums.Length == 1) 
+            {
+                return false; 
+            
+            }
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } 
     }
 }
