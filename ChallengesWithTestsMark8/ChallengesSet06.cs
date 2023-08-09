e2ad9ccf6a7bc7214878d50fdb26c1a8d41812c8
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-08-08 22:09:29 -0400

    Completed ChallengeSet06

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -44,17 +44,100 @@ namespace ChallengesWithTestsMark8
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            
+            int index = -1;
+            bool uniqueIndex;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                uniqueIndex = true;
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[i] == str[j] && i != j)
+                    {
+                        uniqueIndex = false;
+                    }
+                }
+
+                if (uniqueIndex == true)
+                {
+                    index = i;
+                }
+            }
+
+            return index;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int count = 1;
+            int currentCount = 1;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    currentCount = 1;
+                }
+
+                if (currentCount > count)
+                {
+                    count = currentCount;
+                }
+            }
+
+            return count;
+            /*int count = 0;
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentCount = 1;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] != numbers[j])
+                    {
+                        break;
+                    }
+                    currentCount++;
+                }
+
+                if (currentCount > count)
+                {
+                    count = currentCount;
+                }
+            }
+
+            return count;*/
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            List<double> result = new List<double>();
+
+            if (elements == null || n <= 0 || n > elements.Count)
+            {
+                return result.ToArray();
+            }
+
+            
+            for (int i = n -1; i < elements.Count; i += n)
+            {
+
+                result.Add(elements[i]);
+                
+            }
+            return result.ToArray();
         }
     }
 }
